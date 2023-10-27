8af3ab93010905b275f533d962f6ee90e17a585e
BrajaBW <brajabuanawidodo93@gmail.com>
2023-10-28 00:33:23 +0700

    update

---
+++ latihan-web-server/go.mod
@@ -0,0 +1,3 @@
+module latihanwebserver
+
+go 1.21.1
---
+++ latihan-web-server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("about called.....")
+	tentang := "ini Adalah halaman About"
+	w.Write([]byte(tentang))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("index called.....")
+	halaman := "ini Adalah halaman index"
+	w.Write([]byte(halaman))
+}
+
+func handleRoute() {
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/index", index)
+}
+
+func starServer(port string) {
+	handleRoute()
+	fmt.Println("server running at port", port)
+	http.ListenAndServe(port, nil)
+}
+
+func main() {
+
+	port := ":4345"
+	starServer(port)
+
+}
